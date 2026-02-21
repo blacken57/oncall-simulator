ba0604b04655a2daecb670659c36761608d0933b
Abhishek Roy <abhishekriju57@gmail.com>
2026-02-21 15:55:24 +0530

    chore: add wrangler.jsonc and refine cloudflare adapter config

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,13 @@ import adapter from '@sveltejs/adapter-cloudflare';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      // See https://kit.svelte.dev/docs/adapter-cloudflare#options
+      routes: {
+        include: ['/*'],
+        exclude: ['<all>']
+      }
+    })
   }
 };
 
