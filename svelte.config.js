88e25fe1fea9106cf9a73ba8b193bfb1fcd7825d
Abhishek Roy <abhishekriju57@gmail.com>
2026-03-02 16:17:46 +0530

    deploy: add start script and switch to node adapter for Dokploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,9 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      // See https://kit.svelte.dev/docs/adapter-cloudflare#options
-      routes: {
-        include: ['/*'],
-        exclude: ['<all>']
-      }
-    })
+    adapter: adapter()
   }
 };
 
